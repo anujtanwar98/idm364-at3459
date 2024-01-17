edfc23d9ddbbdf28508131d3ed7ddf5ea4cbd312
Anuj Tanwar <91279830+anujtanwar98@users.noreply.github.com>
2024-01-17 17:02:14 -0500

    styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,8 @@
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+</svelte:head>
 <script>
 	import Header from './Header.svelte';
 	import './styles.css';
